5145df93ba25aebfe2c7abf6f97fe71a951c84fc
MWaleedNasir <m040166mn@ksbl.edu.pk>
2023-05-05 20:24:17 +0500

    Have created new functions like merge file and read_csv file. Have also included main execution part of code where most of the functions are being called. Tried to build a type of analysis simulator

--- python-project.py
+++ python-project.py
@@ -1,31 +1,63 @@
-# import numpy as np
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+import random
 import os
+import time
+from data_cleaning_script import data_cleaning_file
+import sys
+
+"""
+Funtions To Read CSV file & Data Cleaning
+Author: Waleed Nasir
+"""   
+def merge_file(filename1 , filename2, how, on):
+    #Merging Both Files
+    data_file = filename1.merge(filename2, how=how, on=on)
+    return data_file
 
 def read_csv_file(filename):
     current_directory = os.getcwd()
     dataframe = pd.read_csv(os.path.join(current_directory,filename+".csv"))
     return dataframe
 
-# print(restaurants_df.head(10))
-# print(restaurants_df.tail())
-# print(restaurants_df.info())
-# print(restaurants_df.shape)
-# print(restaurants_df.describe())
-
-# print(restaurants_menu_df.head(10))
-# print(restaurants_menu_df.tail())
-# print(restaurants_menu_df.info())
-# print(restaurants_menu_df.shape)
-# print(restaurants_menu_df.describe())
-
+"""Main Execution Part of Code"""
 
 if __name__ == "__main__":
-    filename = input("Enter File Name: ")
-    df = read_csv_file(filename)
-    # print(df.head(10))
-    # print(df.tail())
-    # print(df.info())
-    # print(df.shape)
-    print(df.describe())
-    
+    running = True;
+    while running:
+        print("*************************************************")
+        print("****WELCOME TO EXPLORATORY ANALYSIS SIMULATOR****")
+        print("*************************************************")
+        print("Following are the commands to execute \n1) Get list of files (Enter list) \n2) Exit (Press 0)")
+        command_1 = input("Enter Command: ")
+        
+        if command_1 == 'list':
+            print(os.listdir())
+            print("Enter csv file names from above list to load into data frames (Write only file names with space-seperated): ")
+            file_name1, file_name2 = input("").split()
+
+            print("Loading CSV Files........")
+            file_name1 = read_csv_file(file_name1)
+            file_name2 = read_csv_file(file_name2)
+            time.sleep(2)
+
+            print("Cleaning Data Files........")
+            restaurants_data, restaurant_menu_data = data_cleaning_file(file_name1, file_name2)
+            time.sleep(2)
+            # print(restaurants_data)
+            # print(restaurant_menu_data)
+
+            print("Merging Data Files........")
+            modified_data_file = merge_file(restaurants_data,restaurant_menu_data,'left','restaurant_id')
+            modified_data_file.to_csv('modified_data_file.csv')
+            time.sleep(2)
+
+            print("File Saved in Local Directory.")
+            sys.exit()
+
+        elif command_1 == '0':
+            print("Good Bye!")
+            sys.exit()    
